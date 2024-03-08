694011d644c95319d61f8b2978b4560d89375c6b
unistu-code <125600595+unistu-code@users.noreply.github.com>
2024-03-08 22:53:16 +0500

    Create App.java

---
+++ App.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+ 
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+     
+  Parent root;
+try 
+{
+    root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+    Scene scene = new Scene(root);
+  
+    primaryStage.setTitle("Hello World!");
+    primaryStage. initStyle(StageStyle.UNDECORATED);
+
+    Image appIcon=new Image("weather-app.png");
+    primaryStage.getIcons().add(appIcon);
+
+
+
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+
+} catch (IOException e) {
+}
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+// updated
