38baf7438ca958db5c75fbfb1329b0047d74ddd8
HamzaNaveed19 <125171819+HamzaNaveed19@users.noreply.github.com>
2024-04-28 20:45:20 +0500

    Add files via upload

--- Database.java
+++ Database.java
@@ -4,10 +4,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Database implements AbstractDB{
-    private static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/FiveDayForecast", "root", "ahad123");
-    }
 
     private static boolean cityExists(String city, Connection con) throws SQLException {
         String checkCitySql = "SELECT COUNT(*) FROM City WHERE City = ?";
@@ -31,7 +27,7 @@ public class Database implements AbstractDB{
     }
     @Override
     public void storeFiveDayData(String city, JSONObject fiveDayForecast) {
-        try (Connection con = getConnection()) { 
+        try (Connection con =SQLConnection.getConnection()) { 
             checkAndCleanDatabase();
             if (!cityExists(city, con)) {
                 insertCity(city, con);
@@ -62,13 +58,15 @@ public class Database implements AbstractDB{
             e.printStackTrace();
         }
     }
+ 
+
+    @SuppressWarnings("unchecked")
     @Override
     public JSONArray getFiveDaysData(String city) {
         JSONArray forecastsArray = new JSONArray(); 
-        try (Connection con = getConnection()) {
+        try (Connection con = SQLConnection.getConnection()) {
             checkAndCleanDatabase();
             if (!cityExists(city, con)) {
-                
                 System.out.println("City does not exist in the database.");
                 return forecastsArray;
             }
@@ -78,12 +76,12 @@ public class Database implements AbstractDB{
                 
                 try (ResultSet rs = pstmt.executeQuery()) {
                     while (rs.next()) {
-                        JSONObject forecastObject = new JSONObject(); 
+                        JSONObject forecastObject = new JSONObject();
                         forecastObject.put("City", rs.getString("City"));
-                        forecastObject.put("ForecastDate", rs.getString("ForecastDate"));
-                        forecastObject.put("MaxTemp", rs.getDouble("MAX_TEMP"));
-                        forecastObject.put("MinTemp", rs.getDouble("MIN_TEMP"));
-                        forecastObject.put("Description", rs.getString("Weather_Description"));
+                        forecastObject.put("date", rs.getString("ForecastDate"));
+                        forecastObject.put("temp_max", rs.getDouble("MAX_TEMP"));
+                        forecastObject.put("temp_min", rs.getDouble("MIN_TEMP"));
+                        forecastObject.put("weather_description", rs.getString("Weather_Description"));
                         forecastsArray.add(forecastObject);
                     }
                 }
@@ -99,7 +97,8 @@ public class Database implements AbstractDB{
     @Override
     public  void storeThreeHourly(String city, JSONObject threeHourly) {
       
-        try (Connection con = getConnection()) {
+        try (Connection con = SQLConnection.getConnection()) {
+           
             if (!cityExists(city, con)) {
                 insertCity(city, con);
             }
@@ -109,12 +108,13 @@ public class Database implements AbstractDB{
             try (PreparedStatement pstmt = con.prepareStatement(insertSql)) {
                 for (int i = 0; i < forecastArray.size(); i++) {
                     JSONObject jsonObject = (JSONObject) forecastArray.get(i);
-                    String dateTime = (String) jsonObject.get("date_time"); // Correctly casted to String
-                    double temperature = (Double) jsonObject.get("temperature"); // Correctly casted to Double
+                    String dateTime = (String) jsonObject.get("date_time"); 
+                    double temperature = (Double) jsonObject.get("temperature");
                     
                     pstmt.setString(1, city);
                     pstmt.setString(2, dateTime);
                     pstmt.setDouble(3, temperature);
+                    
                     pstmt.executeUpdate();
                 }
             }
@@ -122,13 +122,14 @@ public class Database implements AbstractDB{
             e.printStackTrace();
         }
      }
-     @Override
+     @SuppressWarnings("unchecked")
+    @Override
      public  JSONArray getThreeHourlyData(String city) {
         JSONArray forecastArray = new JSONArray();
-       
+        
         String selectSql = "SELECT Date_Time, Temperature FROM HourlyForecasts WHERE City = ?";
         
-        try (Connection con = getConnection();
+        try (Connection con = SQLConnection.getConnection();
              PreparedStatement pstmt = con.prepareStatement(selectSql)) {
                 if (!cityExists(city, con)) {
                     System.out.println("City does not exist in the database.");
@@ -137,7 +138,6 @@ public class Database implements AbstractDB{
             pstmt.setString(1, city);
             
             try (ResultSet rs = pstmt.executeQuery()) {
-            
                 while (rs.next()) {
                     JSONObject forecast = new JSONObject();
                     forecast.put("date_time", rs.getString("Date_Time"));
@@ -153,12 +153,11 @@ public class Database implements AbstractDB{
     }
     @Override
     public void storeGasesComponent(String city, JSONObject gasesComp) {
-        try (Connection con = getConnection()) { 
+        try (Connection con = SQLConnection.getConnection()) { 
             if (!cityExists(city, con)) {
                 insertCity(city, con);
             }
     
-            // Prepare the SQL statement for inserting air pollutants data
             String insertSql = "INSERT INTO AirPollutantsData (City, NO, NO2, O3, SO2, PM2_5, PM_10, NH3, CO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
             try (PreparedStatement pstmt = con.prepareStatement(insertSql)) {
                 pstmt.setString(1, city);
@@ -171,7 +170,6 @@ public class Database implements AbstractDB{
                 pstmt.setDouble(8, toDouble(gasesComp.get("nh3")));
                 pstmt.setDouble(9, toDouble(gasesComp.get("co")));
                 
-                // Execute the insert statement
                 pstmt.executeUpdate();
             }
         } catch (Exception e) {
@@ -179,33 +177,28 @@ public class Database implements AbstractDB{
         }
     }
     
-    // Helper method to safely convert to Double
     private static double toDouble(Object value) {
         if (value != null) {
             return Double.parseDouble(value.toString());
         }
         return 0.0;
     }
+    @SuppressWarnings("unchecked")
     @Override
     public JSONArray getGasesComponent(String city) {
         JSONArray gasesArray = new JSONArray();
         
-        // SQL query to select air pollutants data for a specific city
         String selectSql = "SELECT NO, NO2, O3, SO2, PM2_5, PM_10, NH3, CO FROM AirPollutantsData WHERE City = ?";
         
-        try (Connection con = getConnection(); // Assumes getConnection() provides a valid database connection
-             PreparedStatement pstmt = con.prepareStatement(selectSql)) {
+        try (Connection con = SQLConnection.getConnection(); 
+         PreparedStatement pstmt = con.prepareStatement(selectSql)) {
             
-            // Set the city parameter in the SQL query
             pstmt.setString(1, city);
             if (!cityExists(city, con)) {
-                // If the city does not exist, handle appropriately
                 System.out.println("City does not exist in the database.");
                 return gasesArray;
             }
-            // Execute the query
             try (ResultSet rs = pstmt.executeQuery()) {
-                // Iterate over the result set and add each set of gas components to the gasesArray
                 while (rs.next()) {
                     JSONObject gasesComponent = new JSONObject();
                     gasesComponent.put("no", rs.getDouble("NO"));
@@ -227,20 +220,17 @@ public class Database implements AbstractDB{
     }
     @Override
     public  void storeAQIComponent(String city, long AQI) {
-        // SQL statement to insert the AQI data
         String insertSql = "INSERT INTO AirQualityIndex (City, AQI) VALUES (?, ?)";
         
-        try (Connection con = getConnection(); // Assumes getConnection() provides a valid database connection
+        try (Connection con = SQLConnection.getConnection(); 
              PreparedStatement pstmt = con.prepareStatement(insertSql)) {
             
-            // Set the parameters for the prepared statement
             pstmt.setString(1, city);
-            pstmt.setDouble(2, AQI); // Even though AQI is a long, it's safe to use setDouble for compatibility
-            
+            pstmt.setDouble(2, AQI); 
             // Execute the insert statement
             pstmt.executeUpdate();
             
-            System.out.println("AQI data stored successfully for " + city);
+          //  System.out.println("AQI data stored successfully for " + city);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to store AQI data for " + city);
@@ -248,55 +238,50 @@ public class Database implements AbstractDB{
     }
     @Override
     public  double getAQIForCity(String city) {
-        // SQL query to select the AQI for a specific city
         String selectSql = "SELECT AQI FROM AirQualityIndex WHERE City = ?";
         
-        try (Connection con = getConnection(); // Assumes getConnection() provides a valid database connection
+        try (Connection con = SQLConnection.getConnection(); 
              PreparedStatement pstmt = con.prepareStatement(selectSql)) {
                 if (!cityExists(city, con)) {
-                    // If the city does not exist, handle appropriately
                     System.out.println("City does not exist in the database.");
                     return 0;
                 }
-            // Set the city parameter in the SQL query
             pstmt.setString(1, city);
             
-            // Execute the query
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
                     return rs.getDouble("AQI"); 
+                } else {
                     System.out.println("No AQI data found for " + city);
-                    return -1;
+                    return -1; // Indicate that no data was found
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Failed to retrieve AQI data for " + city);
-            return -1;
+            return -1; 
         }
     }
-
-public static void checkAndCleanDatabase() throws SQLException, ClassNotFoundException {
-    try (Connection conn = getConnection();
-          Statement stmt = conn.createStatement()) {
-
-          conn.setAutoCommit(false);
     
+
+    /////////////////////////
+public static void checkAndCleanDatabase() throws SQLException, ClassNotFoundException {
+    try (Connection conn = SQLConnection.getConnection();
+          Statement stmt = conn.createStatement()) {
+             
+          conn.setAutoCommit(false);
+            
           ResultSet rs = stmt.executeQuery("SELECT savedDate FROM StoredDate LIMIT 1");
           LocalDate currentDate = LocalDate.now();
           if (rs.next()) {
-
           java.sql.Date storedDate = rs.getObject("savedDate", java.sql.Date.class);
 
 
                 
-        
                 if (!storedDate.toLocalDate().equals(currentDate)) {
-            
                     int deletedCityRows = stmt.executeUpdate("DELETE FROM City");
                     System.out.println("Deleted rows from City table: " + deletedCityRows);
                 
-                 
                     int deletedDateRows = stmt.executeUpdate("DELETE FROM StoredDate");
                     System.out.println("Deleted rows from StoredDate table: " + deletedDateRows);
                 
@@ -305,18 +290,13 @@ public static void checkAndCleanDatabase() throws SQLException, ClassNotFoundExc
                     insertStmt.setDate(1, java.sql.Date.valueOf(currentDate));
 
 
-                   
                     insertStmt.executeUpdate();
                     System.out.println("Inserted current date into StoredDate table.");
-                } else {
-                    System.out.println("The dates match. No action taken.");
                 }
                 
             } else {
                 System.out.println("No stored date found.");
-                
                 PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO StoredDate (savedDate) VALUES (?)");
-
                 insertStmt.setDate(1, java.sql.Date.valueOf(currentDate));
 
                 insertStmt.executeUpdate();
@@ -327,17 +307,28 @@ public static void checkAndCleanDatabase() throws SQLException, ClassNotFoundExc
             
             conn.setAutoCommit(true);
         } catch (SQLException | ClassNotFoundException e) {
-           
             System.err.println("An error occurred, rolling back changes.");
-            try (Connection conn = getConnection()) {
+            try (Connection conn = SQLConnection.getConnection()) {
                 if (conn != null) {
                     conn.rollback();
                 }
             } catch (SQLException ex) {
                 System.err.println("Error during rollback: " + ex.getMessage());
             }
-            throw e; // Rethrow the exception after handling
+            throw e; 
         }
 }
 
+    // public static void main(String[] args) {
+    //     String city = "Lahore"; // Replace "SampleCity" with the name of the city you want to query
+    //     Database d1=new Database();
+    //     // Assuming getGasesComponent is defined in the same class
+    //     JSONArray gasesComponents =d1.getFiveDaysData(city);
+    //     if(gasesComponents.isEmpty()){
+    //         System.out.println("Hamza");    
+    //     }
+    //    // double AQi=getAQIForCity(city);
+    //     // Print the resulting JSONArray to the console
+    //     System.out.println(gasesComponents);
+    // }
 }
